e56813e4e556ce0cc438a66006a4d636392ead1f
The Mist <mist.zzh@gmail.com>
2025-01-18 18:00:01 +0800

    [Chore] change filetype of commitlint config file to yml (#65)

--- commitlint.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  // This line config will read the NPM package named "commitlint-config-wizardoc", so please make sure you have installed it before config this line.
-  extends: ["wizardoc"],
-  rules: [
-      "header-max-length": [2, "always", 100],
-  ]
-};
