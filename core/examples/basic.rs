0aff9fe51c79673da1d53fd02560ec88b738c832
Mist <mist.zzh@gmail.com>
2025-03-15 17:23:34 +0800

    [Chore] rename simple to basic

--- core/examples/basic.rs
+++ core/examples/basic.rs
@@ -3,13 +3,14 @@ use codesnap::config::{CodeBuilder, CodeSnap, Content};
 pub fn main() -> anyhow::Result<()> {
     let code_content = Content::Code(
         CodeBuilder::default()
-            .content(r#"print "Hello, World!""#)
-            .language("python")
+            .content(r#"pub fn main() {}"#)
+            .language("rust")
             .build()?,
     );
 
     let snapshot = CodeSnap::from_default_theme()?
         .content(code_content)
+        .fonts_folder("/Users/zhanhaozhao/repositories/codesnap/core/assets/foo")
         .build()?
         .create_snapshot()?;
 
